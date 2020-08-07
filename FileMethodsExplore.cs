bd4c853cd6ac6dffaf9c8b2b7b2435f27a4b7d25
ssinghai997 <68274724+ssinghai997@users.noreply.github.com>
2020-08-07 18:40:21 +0530

    Add files via upload

---
+++ FileMethodsExplore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace ConFileHandling
+{
+   
+    class FileMethodsExplore
+    {
+        FileStream fs;
+        
+        
+        static void Main()
+        {
+            
+            //FileStream fs = File.Create("fileNew.txt");
+            Console.WriteLine("New File is created");
+            File.WriteAllText("fileText.txt", "This is the demo of Write ALl Text");
+            Console.WriteLine("Text is appended");
+            File.AppendAllText("fileText.txt", "Now I have appended the text to the existing text file");
+            Console.WriteLine("All text has been read");
+            File.ReadAllText("fileText.txt");
+            Console.WriteLine("copying text from 'filetext' to 'fileText2'");
+            //File.Copy("fileText.txt", "fileText2.txt");
+            Console.WriteLine("fileText1 id deleted");
+            File.Delete("fileText1.txt");
+
+            Console.WriteLine("Moving the file from bin>debug to new folder");
+            File.Move(@"D:\file3.txt", @"E:\fileTest\");
+
+            //File Exists Method I
+
+            bool b = File.Exists("fileNew.txt");
+            if (b)
+            {
+                Console.WriteLine("file exists");
+            }
+            else
+            {
+                Console.WriteLine("file does not exist.");
+            }
+
+            //File Exists Method II
+
+            
+            Console.WriteLine(File.Exists("file8.txt") ? "File exists." : "File does not exist.");
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ FileUtilityClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConFileHandling
+{
+    class FileUtilityClasses
+    {
+        static void Main()
+        {
+            //FileStream fs = new FileStream(@"C:\Users\WIN10\OneDrive\Documents\july27th.txt", FileMode.Create,FileAccess.Write);
+            //StreamWriter sw = new StreamWriter(fs);
+            //sw.WriteLine("We have created our first file using filestream ");
+            //sw.WriteLine("FileStream is an utility class of Input Output namespace");
+            //sw.WriteLine("FileStream inherits from the abstract class Stream");
+            //sw.Close();
+            //fs.Close();
+            //Console.WriteLine("Data written in the file");
+            FileStream fs = null;
+            StreamWriter sw = null;
+            try
+            {
+                new FileStream(@"D:\file2.txt", FileMode.Open, FileAccess.Read);
+                new StreamWriter(fs);
+                sw.WriteLine("We have created our first file using filestream ");
+                sw.WriteLine("FileStream is an utility class of Input Output namespace");
+                sw.WriteLine("FileStream inherits from the abstract class Stream");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            finally
+            {
+                Console.WriteLine("data written");
+                sw.Close();
+                fs.Close();
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
